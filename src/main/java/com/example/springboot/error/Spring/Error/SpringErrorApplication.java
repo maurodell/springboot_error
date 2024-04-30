594f99b0f16f355912a4026bd655e3ac7f66b311
Mauro <delloliomauro@gmail.com>
2024-04-30 11:48:43 +0200

    first

---
+++ src/main/java/com/example/springboot/error/Spring/Error/SpringErrorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.error.Spring.Error;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/error/Spring/Error/SpringErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.error.Spring.Error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
