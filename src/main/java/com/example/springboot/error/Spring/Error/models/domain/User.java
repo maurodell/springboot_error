13a10641bbfe80c4dae309602ac4c66e3382f504
Mauro <delloliomauro@gmail.com>
2024-04-30 14:26:16 +0200

    createUser

---
+++ src/main/java/com/example/springboot/error/Spring/Error/models/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.springboot.error.Spring.Error.models.domain;
+
+public class User {
+    private Long id;
+    private String name;
+    private String lastname;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String lastname) {
+        this.id = id;
+        this.name = name;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
